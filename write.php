108b917e10b38e6e4287349f8acbb155a1a690cd
lifexpand <46619462+lifexpand@users.noreply.github.com>
2022-11-03 02:37:11 +0900

    Update write.php

--- write.php
+++ write.php
@@ -24,30 +24,30 @@
 <body>
     <?php
     session_start();
-    if (!isset($_SESSION['userid'])) {
+    if (!isset($_SESSION['userid'])) { //로그아웃 되어있는 경우
         ?>
         <script>
             alert("권환 없음");
-            location.replace("<?php echo "./content.php" ?>");
+            location.replace("<?php echo "./content.php" ?>"); //권환이 없으므로 다시 content 페이지로 이동
         </script>
     <?php
     }
     ?>
-    <form method="post" action="write_action.php">
+    <form method="post" action="write_action.php"> //input들을 post 형식으로 wrtie_action.php에 전달
         <table class="tab" align=center width=auto>
             <tr>
                 <td>name</td>
-                <td><input type="hidden" name="name" value="<?= $_SESSION['username'] ?>"><?= $_SESSION['username'] ?></td>
-            </tr>
+                <td><input type="hidden" name="name" value="<?= $_SESSION['username'] ?>"><?= $_SESSION['username'] ?></td> //username을 출력하고 input data로 넣음
+            </tr> 
             <tr>
                 <td>title</td>
-                <td><input type=text name=title size=80></td>
+                <td><input type=text name=title size=80></td> //title 입력 칸
             </tr>
             <tr>
                 <td>content</td>
-                <td><textarea name=content cols=75 rows=20></textarea></td>
+                <td><textarea name=content cols=75 rows=20></textarea></td> //content 입력 칸
             </tr>
-            <input type="hidden" name="id" value="<?= $_SESSION['userid'] ?>">
+            <input type="hidden" name="id" value="<?= $_SESSION['userid'] ?>"> //userid도 input data로 
         </table>
              <center>
                 <input style="height:30px; width: 120px; font-size:20px;" type="submit" value="submit">
