da40104c7e2a35a83a90c7894d54b61348000acd
lifexpand <46619462+lifexpand@users.noreply.github.com>
2022-11-03 02:25:33 +0900

    Update register.php

--- register.php
+++ register.php
@@ -7,10 +7,10 @@
 
 <body>
     <div align="center">
-        <form method='post' action='register_action.php'>
-            <p>NAME <input name="name" type="text"></p>
-            <p>ID   <input name="id" type="text"></p>
-            <p>PW   <input name="pw" type="password"></p>
+        <form method='post' action='register_action.php'> //input data를 post 형식으로 register_action.php에게 전달
+            <p>NAME <input name="name" type="text"></p> //이름 입력 칸
+            <p>ID   <input name="id" type="text"></p> //id 입력 칸
+            <p>PW   <input name="pw" type="password"></p> //password 입력 칸
             <input type="submit" value="submit">
         </form>
     </div>
