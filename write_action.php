11cb53662518429742c72193d6c9e2145848a772
lifexpand <46619462+lifexpand@users.noreply.github.com>
2022-11-03 02:38:38 +0900

    Update write_action.php

--- write_action.php
+++ write_action.php
@@ -6,22 +6,22 @@ $title = $_POST['title'];
 $content = $_POST['content'];
 $name = $_POST['name'];               
         
-$query = "INSERT INTO board (number, title, content, username, id) values(NULL, '$title', '$content', '$name', '$id')";
+$query = "INSERT INTO board (number, title, content, username, id) values(NULL, '$title', '$content', '$name', '$id')"; //post 형식으로 입력받은 값들을 board에 넣는 쿼리
 
 $result = mysqli_query($link, $query);
-if ($result) {
+if ($result) { //write에 성공한 경우
     ?> 
     <script>
         alert("<?php echo "등록 성공" ?>");
-        location.replace("<?php echo "./content.php"?>");
+        location.replace("<?php echo "./content.php"?>"); //content 페이지로 이동
     </script>
     <?php
 } 
-else {
+else { //wrtie에 실패한 경우
     ?>
     <script>
         alert("<?php echo "등록 실패" ?>");
-        location.replace("<?php echo "./content.php"?>");
+        location.replace("<?php echo "./content.php"?>"); //content 페이지로 
     </script>
     <?php
 }
