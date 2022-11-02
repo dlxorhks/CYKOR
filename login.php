701b675feaad22e114b45a89b35784b44c3a8681
lifexpand <46619462+lifexpand@users.noreply.github.com>
2022-11-03 02:14:33 +0900

    Update login.php

--- login.php
+++ login.php
@@ -6,12 +6,12 @@
 
 <body>
     <div align='center'> 
-        <form method='post' action='login_action.php'>
-            <p>ID <input name="id" type="text"></p>
-            <p>PW<input name="pw" type="password"></p>
+        <form method='post' action='login_action.php'> //post 형식으로 login_action.php에게 input 데이터를 줌
+            <p>ID <input name="id" type="text"></p> //id를 받아오는 칸
+            <p>PW<input name="pw" type="password"></p> //password를 받아오는 칸
             <input type="submit" value="login">
         </form>
-        <button onclick="location.href='./register.php'">register</button>
+        <button onclick="location.href='./register.php'">register</button> //회원 가입 페이지로 넘어가는 버튼
     </div>
 </body>
 
