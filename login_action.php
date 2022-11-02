51bdf2e8f62bb131f793cd47f0ad86afa6cd7c95
lifexpand <46619462+lifexpand@users.noreply.github.com>
2022-11-03 02:18:17 +0900

    Update login_action.php

--- login_action.php
+++ login_action.php
@@ -5,23 +5,23 @@ $link = mysqli_connect("127.0.0.1", "root", "1234", "web1");
 
 $id = $_POST['id'];
 $pw = $_POST['pw'];
-$query = "select * from member where id='$id'";
+$query = "select * from member where id='$id'"; //post 형식으로 받은 id 변수와 동일한 id를 가지고 있는 member table을 가져오는 쿼리
 $result = $link->query($query);
 
 $row = mysqli_fetch_assoc($result);
-if ($row['password'] == $pw) {
-    $_SESSION['userid'] = $id;
-    $_SESSION['username'] = $row['name'];
+if ($row['password'] == $pw) { //입력된 비밀번호와 member table의 password가 동일할 경우
+    $_SESSION['userid'] = $id; //userid 세션 변수에 member table의 id를 넣음
+    $_SESSION['username'] = $row['name']; // username 세션 변수에 member table name을 넣음
     if (isset($_SESSION['userid'])) {
         ?> <script>
-            location.replace("./content.php");
+            location.replace("./content.php"); //content 페이지로 복귀
         </script>
         <?php
     } 
     else {
         ?> <script>
-            alert("잘못된 입력");
-            history.back(); 
+            alert("잘못된 입력"); 
+            history.back(); //전 페이지로 이동(login.php)
         </script>
         <?php
     }
