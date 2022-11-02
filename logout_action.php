3f92d829d05f2cbe08d3c0f80809b5d7f5dd2a66
lifexpand <46619462+lifexpand@users.noreply.github.com>
2022-11-03 02:20:30 +0900

    Update logout_action.php

--- logout_action.php
+++ logout_action.php
@@ -1,10 +1,9 @@
 <?php
 session_start();
 $result = session_destroy();
-if ($result) {
-    ?><script>
-        history.back();
+
+if ($result) {  //로그아웃에 성공했을 경우
+?><script>
+        history.back(); //전 페이지로 이동()
     </script>
-    <?php 
-} 
-?>
+<?php } ?>
